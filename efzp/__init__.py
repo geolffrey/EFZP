4639c0188a04f755eb550ea3ee06b8d674cac91e
Peter <peter.degroff@platform46.com>
2011-11-04 16:10:54 +1100

    quick packaging job

---
+++ efzp/__init__.py
@@ -0,0 +1,1 @@
+from EFZP import *
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+try:
+    long_description = open('README').read()
+except:
+    long_description = ''
+
+setup(
+    name = 'efzp',
+    version = '1.0',
+    description = 'Describe an email in terms of salutation, body, signature, reply text etc.',
+    author = 'Dave Trindall',
+    url = 'https://github.com/Trindaz/EFZP',
+    packages = ['efzp'],
+    license = 'Apache License 2.0',
+)
+
